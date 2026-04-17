f75aef3a4d7b21c919cee44bda2028411e488ad4
Matteo Zapparoli <zapparoli.matteo@gmail.com>
2026-04-17 11:09:24 +0200

    Update SimpleGen.java

--- src/Java/org/mizar64/test/SimpleGen.java
+++ src/Java/org/mizar64/test/SimpleGen.java
@@ -5,14 +5,14 @@ import org.mizar64.Mizar64;
 /**
  * Simple mizar64 generator.
  * Used to compare Miza64 java version vs Mizar64 C version
- * @author ZappMa1
+ * @author Matteo Zapparoli
  * 7 apr 2026
  */
 public class SimpleGen {
 
     private static void usage() {
         System.out.println("SimpleGen - Print on stdout random number using miza64 PRNG");
-        System.out.println("Usage: simplegen <seed c> <seed x> <n>");
+        System.out.println("Usage: simplegen <seed> <n>");
     }
     
     public static void main(String[] args) {
@@ -21,15 +21,14 @@ public class SimpleGen {
             System.exit(0);
         }
         
-        if(args.length != 3) {
-            System.err.println("*** Error: invalid number");
+        if(args.length != 2) {
+            System.err.println("*** Error: invalid arguments");
         }
         
         long checksum = 0L;
-        long c = Long.parseLong(args[0]);
-        long x = Long.parseLong(args[1]);
-        int n = Integer.parseInt(args[2]);
-        Mizar64 mizar64 = new Mizar64(c, x);
+        long seed = Long.parseLong(args[0]);
+        int n = Integer.parseInt(args[1]);
+        Mizar64 mizar64 = new Mizar64(seed);
         while(n-- > 0) {
             long rand = mizar64.nextLong();
             System.out.printf("%016X\n", rand);
@@ -38,3 +37,4 @@ public class SimpleGen {
         System.out.printf("\nChecksum = %016X\n", checksum);
     }
 }
+
